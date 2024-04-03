812cd8e8dc1708c00f8c44e17e0bd382e92c4d38
dananjanaA <dananjanaA@HQ-IT-PC36>
2024-04-03 16:19:26 +0530

    Initial commit

---
+++ src/app/components/tickets-details/tickets-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './tickets-details.component.html',
+  styleUrl: './tickets-details.component.css'
+})
+export class TicketsDetailsComponent {
+
+}
---
+++ src/app/shared/components/nvbar/nvbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nvbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './nvbar.component.html',
+  styleUrl: './nvbar.component.css'
+})
+export class NvbarComponent {
+
+}
